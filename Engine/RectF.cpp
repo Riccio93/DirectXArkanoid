6b39dc2dc9817500e6f474bdcef0a2925efb1383
Andrea Ricci <andrearicci500@gmail.com>
2022-12-01 11:14:40 +0100

    add rect class

---
+++ Engine/RectF.cpp
@@ -0,0 +1,15 @@
+#include "RectF.h"
+
+RectF::RectF(float _left, float _right, float _top, float _bottom) 
+	: left(_left), right(_right), top(_top), bottom(_bottom) {}
+
+RectF::RectF(const Vec2& topLeft, const Vec2& bottomRight) 
+	: RectF(topLeft.x, bottomRight.x, topLeft.y, bottomRight.y) {}
+
+RectF::RectF(const Vec2& topLeft, float width, float height) 
+	: RectF(topLeft, topLeft + Vec2(width, height)) {}
+
+bool RectF::IsOverlappingWith(const RectF& other) const
+{
+	return right > other.left && left < other.right && bottom > other.top && top < other.bottom;
+}
